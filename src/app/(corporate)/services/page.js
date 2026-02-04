175f923202caeefcb0bd3ee7e623e177a8689a59
David Onyekwere <davidchibuzor.ony@gmail.com>
2026-02-04 11:13:07 +0100

    added pagehero1

--- src/app/(corporate)/services/page.js
+++ src/app/(corporate)/services/page.js
@@ -1,15 +1,16 @@
-import PageHero from "@/components/shared/PageHero/PageHero";
+import Navbar from '@/components/layout/Navbar/Navbar'
+import PageHero from '@/components/shared/PageHero/PageHero'
 
-
-export default function ServicesPage() {
+export default function ServicesPage () {
   return (
     <>
+      <Navbar />
       <PageHero
-        title="Our Services"
-        subtitle="How we can help you"
-        backgroundImage="/images/shared/services1.avif"
+        title='Our Services'
+        subtitle='How we can help you'
+        backgroundImage='/images/shared/services1.avif'
         showShareIcons={true}
       />
     </>
-  );
+  )
 }
